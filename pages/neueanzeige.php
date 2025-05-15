1bd2ecf73b6f01b7ef95f84b72526b2ebd226ebc
Nele M <nelemeyer43@gmail.com>
2025-05-15 14:51:09 +0200

    formular anzeige aufgeben erstellt

--- pages/neueanzeige.php
+++ pages/neueanzeige.php
@@ -1,2 +1,16 @@
-
+<form method="post">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Titel</label>
+        <input class="form-control" type="text" aria-label="default input example">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Beschreibung</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="formFile" class="form-label">Bild</label>
+        <input class="form-control" type="file" id="formFile">
+    </div>
+    <button type="Submit" class="btn btn-primary">Anzeige Aufgeben</button>
+</form>
 
